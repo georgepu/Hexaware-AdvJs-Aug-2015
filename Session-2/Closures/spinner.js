02c0a3f9328e0d2af973d3b89eeb0d496694966a
tkmagesh <tkmagesh77@gmail.com>
2015-08-26 06:25:09 +0530

    Spinner - Assignment

---
+++ Session-2/Closures/spinner.js
@@ -0,0 +1,22 @@
+/*
+create an object and assign it to the variable "spinner"
+
+var spinner = ....
+
+the spinner is expected to exhibit the following behavior
+
+spinner.up() => 1
+spinner.up() => 2
+spinner.up() => 3
+spinner.up() => 4
+
+spinner.down() => 3
+spinner.down() => 2
+spinner.down() => 1
+spinner.down() => 0
+spinner.down() => -1
+
+The variable that is used to track the value should NOT be accessible from outside
+
+*/
+
