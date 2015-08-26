207c667116d75783f9f67bd7d45da8704d3215f6
tkmagesh <tkmagesh77@gmail.com>
2015-08-26 07:05:16 +0530

    memoize

--- Session-2/Closures/primeFinder.js
+++ Session-2/Closures/primeFinder.js
@@ -17,3 +17,38 @@ function getPrimeFinder(){
     }
     return checkPrime;
 }
+
+function add(x,y){
+    console.log("processing ", x , " and ", y);
+    return x + y;
+}
+
+function isPrime(n){
+    console.log('Processing ' , n);
+    if (n <= 3) return true;
+    for(var i=2; i<=(n/2); i++)
+        if (n % i === 0) return false;
+    return true;
+}
+
+function memoize(fn){
+    var cache = {};
+    return function(){
+        var key = JSON.stringify(arguments);
+        if (typeof cache[key] === 'undefined')
+            cache[key] = fn.apply(this, arguments);
+        return cache[key];
+    }
+}
+
+var memoizedAdd = memoize(add);
+memoizedAdd(100,200) //displays 'processing'
+memoizedAdd(200,300) //displays 'processing'
+
+memoizedAdd(100,200) //doesnot display 'processing'
+memoizedAdd(200,300) //doesnot display 'processing'
+
+
+var primeFinder = memoize(isPrime)
+primeFinder(100)
+primeFinder(100);
